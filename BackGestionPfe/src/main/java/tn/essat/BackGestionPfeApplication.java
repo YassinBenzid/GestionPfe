dfc8d001d8bc5576a447d01367094d352f9bf3f3
Yacin Bz <Yacin Bz@DESKTOP-LNVN8BO>
2020-12-31 12:28:22 +0100

    Gestion de pfe mini projet

---
+++ BackGestionPfe/src/main/java/tn/essat/BackGestionPfeApplication.java
@@ -0,0 +1,13 @@
+package tn.essat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGestionPfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGestionPfeApplication.class, args);
+	}
+
+}
